c6ed9a1b909336ea449d7d84d3795001154d20be
kamil <kamilluthor@gmail.com>
2022-07-31 20:50:10 +0300

    2021

---
+++ c/2021/TwoArrays/program.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define N 10
+
+// void func(int array[N], int *)
+// {
+//     int start = N / 2;
+//     for (int i = 0; i < N; i++)
+//     {
+
+//     }
+// }
+
+void Main()
+{
+    int a[N] = {5, 1, 7, 4, 5, 8, 6, 2};
+
+    for (int i = 0; i < N; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    // func(A);
+}
